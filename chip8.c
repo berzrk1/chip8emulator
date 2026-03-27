5c7713d7e66f94fbe5d9e25219d63666f91b27a0
berzrk1 <67083456+berzrk1@users.noreply.github.com>
2026-03-27 06:33:29 -0300

    add opcode skeleton

--- chip8.c
+++ chip8.c
@@ -81,15 +81,204 @@ void emulateCycle(chip8 *chip) {
     // Decode Opcode
     // Read the first byte to get the opcode
     switch (chip->opcode & 0xF000) {
-        // TODO: Implement opcodes
+        // TODO: Implement opcodes (1/35)
+
+    case 0x0000:
+        switch (chip->opcode & 0x000F) {
+        case 0x0000: // 00E0: Clear screen
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x000E: // 00EE: Returns from a subroutine
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        default:
+            printf("Unknown opcode [0x0000]: 0x%X\n", chip->opcode);
+            chip->PC += 2;
+        }
+        break;
+    case 0x1000: // 1NNN: Jumps to address NNN
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0x2000: // 2NNNN: Calls subroutine at NNN
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0x3000: // 3XNN: Skips the next instruction if VX equals NN
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0x4000: // 4XNN: Skips the next instruction if VX does not equals NN
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0x5000: // 5XY0: Skips the next instruction if VX equals VY
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0x6000: // 6XNN: Sets VX to NN
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0x7000: // 7XNN: Adds NN to VX
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0x8000:
+        switch (chip->opcode & 0x000F) {
+        case 0x0000: // 8XY0: Sets VX to the value of VY
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x0001:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x0002:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x0003:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x0004:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x0005:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x0006:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x0007:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        case 0x000E:
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+        default:
+            printf("Unknown opcode [0x8000]: 0x%X\n", chip->opcode);
+            chip->PC += 2;
+        }
+        break;
+
+    case 0x9000: // 9XY0: Skips the next instruction if VX does not equal VY
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
 
     case 0xA000: // ANNN: Sets I to the address NNN
         chip->I = chip->opcode & 0x0FFF;
         chip->PC += 2; // Skip 2 since we read 2 memory address
         break;
 
+    case 0xB000: // BNNN: Jumps to the address NNN plus V0
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0xC000: // CXNN:
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0xD000:
+        printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+        chip->PC += 2;
+        break;
+
+    case 0xE000:
+        switch (chip->opcode & 0x000F) {
+        case 0x000E: // EX9E
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x0001: // EXA1
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        default:
+            printf("Unknown opcode [0xE000]: 0x%X\n", chip->opcode);
+            chip->PC += 2;
+        }
+        break;
+
+    case 0xF000:
+        switch (chip->opcode & 0x00FF) {
+        case 0x0007: // FX07
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x000A: // FX0A
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x0015: // FX15
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x0018: // FX18
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x001E: // FX1E
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x0029: // FX29
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x0033: // FX33
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x0055: // FX55
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        case 0x0065: // FX65
+            printf("Opcode (0x%X) not implemented yet\n", chip->opcode);
+            chip->PC += 2;
+            break;
+
+        default:
+            printf("Unknown opcode [0xF000]: 0x%X\n", chip->opcode);
+            chip->PC += 2;
+        }
+        break;
+
     default:
         printf("Unknown opcode: 0x%X\n", chip->opcode);
+        chip->PC += 2;
+        break;
     }
 
     if (chip->delay_timer > 0)
